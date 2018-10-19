37f012f6479f436d904e0f8b7108456a0b17c5b2
Joe Shepherd <joeshepmedia@gmail.com>
2018-10-19 10:50:11 -0500

    Mild refactor

--- main.js
+++ main.js
@@ -1,51 +1,51 @@
 console.log("howdy")
 
-// // UI element creation functions
-// let elementFactory = (el, content, {id, clazz}, ...children) => {
-//   let element = document.createElement(el)
-//   element.innerHTML = content || null
-//   children.forEach( child => {
-//     element.appendChild(child)
-//   })
-//   element.setAttribute("id", id)
-//   element.setAttribute("class", clazz)
-//   return element
-// }
+// UI element creation functions
+let elementFactory = (el, content, {id, clazz}, ...children) => {
+  let element = document.createElement(el)
+  element.innerHTML = content || null
+  children.forEach( child => {
+    element.appendChild(child)
+  })
+  element.setAttribute("id", id)
+  element.setAttribute("class", clazz)
+  return element
+}
 
-// let foodList = document.querySelector("#foodlist")
-// let localFood;
+let foodList = document.querySelector("#foodlist")
+let localFood;
 
-// // get the initial data from my API
-// fetch("http://localhost:8088/food/")
-//   .then( (foodData) => foodData.json()) //convert json to js
-//   .then( (realData) => { // loop over the converted js array of objects to query 3rd-party API
-//     realData.forEach( (food) => {
-//       localFood = food
-//       fetch(`https://world.openfoodfacts.org/api/v0/product/${food.barcode}.json`)
-//       .then((apiFoodItem) => apiFoodItem.json())
-//       .then((convertedFood) => { // create elements to insert into DOM
-//         let fragment = document.createDocumentFragment()
-//         // Make an h3 element
-//         let title = elementFactory("h3", food.name, {
-//           id: null,
-//           class: "foodTitle"
-//         })
-//         // make a p element to contain the ingredients list
-//         let ingredients = elementFactory("p", convertedFood.product.ingredients_text, {
-//           id: null,
-//           class: "foodIngredients"
-//         })
-//         // Make a list item component composed of the h3 and p elements
-//         let foodListItem = elementFactory("li", null, {id: "foodItem", clazz: "foodItem"}, title, ingredients)
+// get the initial data from my API
+fetch("http://localhost:8088/food/")
+  .then( (foodData) => foodData.json()) //convert json to js
+  .then( (realData) => { // loop over the converted js array of objects to query 3rd-party API
+    realData.forEach( (food) => {
+      localFood = food
+      fetch(`https://world.openfoodfacts.org/api/v0/product/${food.barcode}.json`)
+      .then((apiFoodItem) => apiFoodItem.json())
+      .then((convertedFood) => { // create elements to insert into DOM
+        let fragment = document.createDocumentFragment()
+        // Make an h3 element
+        let title = elementFactory("h3", food.name, {
+          id: null,
+          class: "foodTitle"
+        })
+        // make a p element to contain the ingredients list
+        let ingredients = elementFactory("p", convertedFood.product.ingredients_text, {
+          id: null,
+          class: "foodIngredients"
+        })
+        // Make a list item component composed of the h3 and p elements
+        let foodListItem = elementFactory("li", null, {id: "foodItem", clazz: "foodItem"}, title, ingredients)
 
-//         // Attach the list item to the fragment
-//         fragment.appendChild(foodListItem)
+        // Attach the list item to the fragment
+        fragment.appendChild(foodListItem)
 
-//         // Insert food item into the DOM
-//         foodList.appendChild(fragment)
-//       })
-//     })
-//   })
+        // Insert food item into the DOM
+        foodList.appendChild(fragment)
+      })
+    })
+  })
 
 
 // ====================The Promise.all way=========================
